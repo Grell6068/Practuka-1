55d5e78e524f099624dd20e9f5168de2b16d9685
Liza Stadnichenko <6068011grell@gmail.com>
2024-09-19 22:56:33 +0300

    6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int forks = 10;
+    int glasses = 10;
+    int knives = 10;
+    int sum = forks + glasses + knives;
+    
+    cout << "Кількість виделок: " << forks << "\t";
+    cout << "Кількість стаканів: " << glasses << "\t";
+    cout << "Кількість ножів: " << knives << "\t";
+    cout << "Загальна кількість столових приборів: " << sum;
+    
+    
+    return 0;
+}
