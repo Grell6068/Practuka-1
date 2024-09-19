d4a527a8b173a2a721fa843732ce8c11bc791788
Liza Stadnichenko <6068011grell@gmail.com>
2024-09-19 15:00:01 +0300

    5.cpp

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int first = 5;
+    int second = first + 3;
+    int third = first + second;
+
+    cout << "Значення першої змінної: " << first << endl;
+    cout << "Значення другої змінної: " << second << endl;
+    cout << "Значення третьої змінної: " << third << endl;
+
+    return 0;
+}
