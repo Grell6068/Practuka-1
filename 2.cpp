fdbcf34e4c3bc69bd38b132db429c60ba9f076df
Liza Stadnichenko <6068011grell@gmail.com>
2024-09-19 14:53:48 +0300

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 2147483649;
+    unsigned int INT = 4294967296;
+    cout<< number<<"\n";
+    cout<< INT;
+    return 0;
+}
